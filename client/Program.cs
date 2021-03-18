b2807d6abb2c58283709ae374b25d5ec4894175d
foo.bar <foo.bar@lorem.ipsum>
2021-03-18 21:00:00 +0200

    Removed test code

--- client/Program.cs
+++ client/Program.cs
@@ -9,15 +9,8 @@ namespace client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            Config config = new Config("config.ini");
-            ServiceDiscoveryClient client = new ServiceDiscoveryClient(config);
             
-            List<GameInfo> games = client.FetchGameList ();
-            GameInfo newGame = client.RegisterGame (ServiceDiscoveryClient.CreateNewGameInstance ("foobar", 12345));
-            client.StartGame (newGame);
-            client.DeregisterGame (newGame);
+
         }
     }
 }
