978064e0a140658837e471eeb769ccd3903b726d
oulu_hillbilly <svaranka@gmail.com>
2021-03-28 14:51:38 +0300

    Disable start game button on lobby if no other players

--- BombPeli/forms/GameLobby.xaml.cs
+++ BombPeli/forms/GameLobby.xaml.cs
@@ -17,83 +17,114 @@ using System.Windows.Shapes;
 
 namespace BombPeli
 {
-	/// <summary>
-	/// Interaction logic for GameLobby.xaml
-	/// </summary>
-	public partial class GameLobby : Page, IChangePage
-	{
+    /// <summary>
+    /// Interaction logic for GameLobby.xaml
+    /// </summary>
+    public partial class GameLobby : Page, IChangePage
+    {
 
-		public delegate void StartGameEventHandler (object sender, GameStartEventArgs e);
-		public delegate void LeaveLobbyEventHandler (object sender, EventArgs e);
-		public event StartGameEventHandler StartGame;
-		public event LeaveLobbyEventHandler LeaveLobby;
+        public delegate void StartGameEventHandler(object sender, GameStartEventArgs e);
+        public delegate void LeaveLobbyEventHandler(object sender, EventArgs e);
+        public event StartGameEventHandler StartGame;
+        public event LeaveLobbyEventHandler LeaveLobby;
 
-		private GameLobbyState lobby;
+        private GameLobbyState lobby;
 
-		public GameLobby () {
-			InitializeComponent ();
-		}
+        public GameLobby()
+        {
+            InitializeComponent();
+        }
 
-		public void Init (State state) {
-			this.lobby = state as GameLobbyState;
+        public void Init(State state)
+        {
+            this.lobby = state as GameLobbyState;
 
-			ListBoxPeers.DataContext = this;
-			ListBoxPeers.ItemsSource = Peers;
-			ViewControls.Children.Clear ();
-			if (lobby.IsHost) {
-				ViewControls.Children.Add (MakeStartButton ());
-			}
-			ViewControls.Children.Add (MakeQuitButton ());
-		}
+            ListBoxPeers.DataContext = this;
+            ListBoxPeers.ItemsSource = Peers;
+            ViewControls.Children.Clear();
+            if (lobby.IsHost)
+            {
+                ViewControls.Children.Add(MakeStartButton());
+            }
+            ViewControls.Children.Add(MakeQuitButton());
+        }
 
-		public void Clear () {
-			lobby = null;
-		}
+        public void Clear()
+        {
+            lobby = null;
+        }
 
-		public State GetState () {
-			return lobby;
-		}
+        public State GetState()
+        {
+            return lobby;
+        }
 
-		public List<PeerInfo> Peers {
-			get { return lobby.Peers; }
-		}
+        public List<PeerInfo> Peers
+        {
+            get { return lobby.Peers; }
+        }
 
-		private Button MakeStartButton () {
-			Button button = new Button ();
-			button.Name = "start";
-			button.Click += start_Click;
-			button.Content = "Start game";
-			button.Margin = new Thickness(10);
-			button.Height = 30;
-			return button;
-		}
+        private Button MakeStartButton()
+        {
+            Button button = new Button();
+            button.Name = "start";
+            button.Click += start_Click;
+            button.Content = "Start game";
+            button.Margin = new Thickness(10);
+            button.Height = 30;
+            button.IsEnabled = false;
+            return button;
+        }
 
-		private Button MakeQuitButton () {
-			Button button = new Button ();
-			button.Name = "quit";
-			button.Click += quit_Click;
-			button.Content = "Leave game";
-			button.Margin = new Thickness (10);
-			button.Height = 30;
-			return button;
-		}
+        private Button MakeQuitButton()
+        {
+            Button button = new Button();
+            button.Name = "quit";
+            button.Click += quit_Click;
+            button.Content = "Leave game";
+            button.Margin = new Thickness(10);
+            button.Height = 30;
+            return button;
+        }
 
-		private void start_Click (object sender, RoutedEventArgs e) {
-			StartGame?.Invoke (this, new GameStartEventArgs (lobby.BombTime));
-		}
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            StartGame?.Invoke(this, new GameStartEventArgs(lobby.BombTime));
+        }
 
-		private void quit_Click (object sender, RoutedEventArgs e) {
-			LeaveLobby?.Invoke (this, e);
-		}
+        private void quit_Click(object sender, RoutedEventArgs e)
+        {
+            LeaveLobby?.Invoke(this, e);
+        }
 
-		public void PeerListChangedHandler (object sender, EventArgs e) {
-			Application.Current.Dispatcher.BeginInvoke ((Action)(() => { DoPeerListHandler (); }));
-		}
+        public void PeerListChangedHandler(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.BeginInvoke((Action)(() => { DoPeerListHandler(); }));
+        }
 
-		public void DoPeerListHandler () {
-			ListBoxPeers.Items.Refresh ();
-		}
+        public void DoPeerListHandler()
+        {
+            ListBoxPeers.Items.Refresh();
+            if (lobby.IsHost)
+            {
+                if (!ListBoxPeers.Items.IsEmpty)
+                {
+                    enableStartButton(true);
+                }
+                else
+                {
+                    enableStartButton(false);
+                }
+            }
+        }
 
+        private void enableStartButton(bool enable)
+        {
+            
+                var btn = ViewControls.Children.OfType<Button>().Single(child => child.Name == "start");
+                btn.IsEnabled = enable;
+            
+        }
 
-	}
+    }
 }
--- client.bat
+++ client.bat
@@ -3,8 +3,8 @@
 set path=%CD%
 
 cd "BombPeli\bin\Debug\net5.0-windows"
-rem start BombPeli.exe "config.ini"
-rem start BombPeli.exe "config2.ini"
+start BombPeli.exe "config.ini"
+start BombPeli.exe "config2.ini"
 start BombPeli.exe "config3.ini"
 start BombPeli.exe "config4.ini"
 
